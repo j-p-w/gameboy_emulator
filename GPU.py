3e116ff4cdcb0bca08bf9d540643fcca9418aa68
BuildTools <unconfigured@null.spigotmc.org>
2017-04-21 18:04:38 -0700

    Added previous_tiles to GPU class to speed up considerably

--- GPU.py
+++ GPU.py
@@ -20,6 +20,21 @@ class GPU:
         self.cycles = 0
         self.memory = memory
         self.counter = 0
+        self.FRAME_SKIP = 1
+
+        self.tile = [
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)],
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)],
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)],
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)],
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)],
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)],
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)],
+            [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)]
+        ]
+
+        self.previous_tiles = [0x100 for i in range(0,0x400)]
+        self.first_render = True
 
     def update(self, cycles_passed):
 
@@ -30,21 +45,18 @@ class GPU:
         self.memory.write(0xFF44, LY)
 
         # Vblank period begin
-        if LY == 0x90:
-            #
-            self.counter += 1
-        else:
-            self.counter += 1
 
         # 70220 clock cycles is one frame
         if self.cycles >= 70220:
             self.cycles = 0
-            self.render_background()
-            for event in pygame.event.get():
-            	if event.type == pygame.QUIT:
-                    pygame.display.quit()
-                    pygame.quit()
-                    sys.exit()
+            if self.counter % self.FRAME_SKIP == 0:
+                self.render_background()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        pygame.display.quit()
+                        pygame.quit()
+                        sys.exit()
+            self.counter += 1
 
     def render_background(self):
 
@@ -54,53 +66,60 @@ class GPU:
         # Read each byte at BG Map Data 1
         for index in range(0, 0x400):
 
-            # Tile data is 16 bytes long
-            tile_data_index = 0x8000 + self.memory.read(0x9800 + index) * 16
+            # Get the current tile
+            current_tile = self.memory.read(0x9800 + index)
 
-            # Get each portion of the tile data in increments of 2 bytes (a row at a time)
-            rows = [[],[],[],[],[],[],[],[]]
-            n = 0
-            for i in range(0,16,2):
+            # Only bother updating if it changed
+            if self.previous_tiles[index] != current_tile:
 
-                byte2 = self.memory.read(tile_data_index + i)
-                byte1 = self.memory.read(tile_data_index + i + 1)
+                # Tile data is 16 bytes long
+                tile_data_index = 0x8000 + current_tile * 16
 
-                # Determine the color of each pixel (2 bits), example:
-                # byte1 = 0b10001111
-                # byte2 = 0b01101001
-                # colors = [0x10, 0x01, 0x01, 0x00, 0x11, 0x10, 0x10, 0x11]
-                colors = [(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0),(0,0,0)]
-                for b in range(0,8):
+                # Get each portion of the tile data in increments of 2 bytes (a row at a time)
+                n = 0
+                for i in range(0,16,2):
 
-                    if byte1 & (0x80 >> b): c1 = 1
-                    else: c1 = 0
+                    byte2 = self.memory.read(tile_data_index + i)
+                    byte1 = self.memory.read(tile_data_index + i + 1)
 
-                    if byte2 & (0x80 >> b): c2 = 1
-                    else: c2 = 0
+                    # Determine the color of each pixel (2 bits)
+                    for b in range(0,8):
 
-                    s = (c2 << 1) + c1
-                    if s == 0:
-                        color = self.white
-                    elif s == 1:
-                        color = self.light_grey
-                    elif s == 2:
-                        color = self.dark_grey
-                    elif s == 3:
-                        color = self.black
-                    else:
-                        color = (255,0,0) # bad tiles are red
-                    colors[b] = color
+                        if byte1 & (0x80 >> b): c1 = 1
+                        else: c1 = 0
 
-                rows[n] = colors
-                n += 1
+                        if byte2 & (0x80 >> b): c2 = 1
+                        else: c2 = 0
 
-            # Copy each row from the tile into the background array
-            for y in range(0,8):
-                for x in range(0,8):
-                    xindex = (index % 32) * 8 + x
-                    yindex = int(index / 32) * 8 + y
-                    self.background_pixels[xindex, yindex] = rows[y][x]
+                        s = (c2 << 1) + c1
 
+                        if s == 0:
+                            color = self.white
+                        elif s == 1:
+                            color = self.light_grey
+                        elif s == 2:
+                            color = self.dark_grey
+                        elif s == 3:
+                            color = self.black
+                        else:
+                            color = (255,0,0) # bad tiles are red
+
+                        self.tile[n][b] = color
+
+                    n += 1
+
+                # Copy each row from the tile into the background array
+                for y in range(0,8):
+                    for x in range(0,8):
+                        xindex = (index % 32) * 8 + x
+                        yindex = int(index / 32) * 8 + y
+                        self.background_pixels[xindex, yindex] = self.tile[y][x]
+
+            # Make sure we update the previous tiles at least once
+            if self.first_render:
+                self.previous_tiles[index] = current_tile
+
+        self.first_render = False
         del self.background_pixels
         self.screen.blit(self.background,(0, -1*self.memory.read(0xFF42)))
         pygame.display.flip()
@@ -112,5 +131,5 @@ class GPU:
 
     def render(self):
 
-        self.screen.blit(self.background,(0,0))
+        self.screen.blit(self.background,(0,-1*self.memory.read(0xFF42)))
         pygame.display.flip()
